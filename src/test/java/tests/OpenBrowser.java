358ddd7e5bde4fbbc9cee28dc47548c04461e3fe
Abhinay Singh Negi <negia394@gmail.com>
2025-02-06 18:16:25 +0530

    Maximize browser

--- src/test/java/tests/OpenBrowser.java
+++ src/test/java/tests/OpenBrowser.java
@@ -5,13 +5,21 @@ import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 public class OpenBrowser {
 
     public static void main(String[] args) {
 
+        ArrayList<String> arguments = new ArrayList<>();
+        arguments.add("--start-maximized");
+
         Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        Page page = browser.newPage();
+
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setArgs(arguments));
+        Page page = browser.newContext(new Browser.NewContextOptions().setViewportSize(null)).newPage();
+
         page.navigate("https://www.google.com");
         System.out.println(page.title());
         page.close();
